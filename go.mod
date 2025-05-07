63679547ed4600eb1676c18efd4dabedf29ebaad
Iván SZKIBA <szkiba@users.noreply.github.com>
2025-05-07 08:44:35 +0000

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/szkiba/hello
+
+go 1.24.0
---
+++ main.go
@@ -0,0 +1,6 @@
+// Package main is the entry point for the program.
+package main
+
+func main() {
+	println("Hello, World!") //nolint:forbidigo
+}
